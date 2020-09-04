e967003413e8699e464a77a977601410cabed7be
Riikka Korolainen <riikka.korolainen@outlook.com>
2020-09-04 11:37:45 +0300

    board with move validator

---
+++ src/main/java/domain/Board.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.regex.*;  
+
+/**
+ * next: modaa lautaa konstruktori testeille + addMove + checkWin ja gittiiii
+ * @author riikoro
+ */
+public class Board {
+    
+    int[][] board;
+
+    public Board() {
+        this.board = new int[8][8];
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 8; j++){
+                board[i][j]=0;
+            }
+        }
+        board[3][3]=2;        
+        board[3][4]=1;
+        board[4][3]=1;
+        board[4][4]=2;
+    }
+    
+    public Board(int[][] initialState){
+        this.board = initialState;
+    }
+
+    public int[][] getBoard() {
+        return board;
+    }
+    
+    /* String representation of board, white unicode U+25CF
+        a b c d e f g h 
+      1| | | | | | | | |
+      2| | | | | | | | |
+      3| | | | | | | | |
+      4| | | |●|○| | | |
+      5| | | |○|●| | | |
+      6| | | | | | | | |
+      7| | | | | | | | |
+      8| | | | | | | | |
+    */
+    public String toString(){
+        String b = "  a b c d e f g h\n";
+        for(int i = 0; i < board.length; i++){
+            b += i+1;
+            for(int j = 0; j < board[0].length; j++){
+                b += "|";
+                if(board[i][j]==0){
+                    b+=" ";
+                }else if(board[i][j]==1){
+                    b+="○";
+                }else{
+                    b+="●";
+                }
+            }
+            b += "|\n";
+        }
+        return b;
+    }
+    
+    public void addMove(int row, int col, int turn){
+        this.board[row][col] = turn;
+    }
+
+    boolean checkMoveValid(String moveStr, int own) {
+        if(checkMoveString(moveStr)){
+            int opponent = own == 1 ? 2 : 1;
+
+            //{rowindex, colindex}
+            int[] move = convertStringToCoordinates(moveStr);
+            int row = move[0];
+            int col = move[1];
+
+            //clockwise from top-left:
+            int[][] directions = {{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
+            for(int i = 0; i < 8; i++){
+                int nextrow = row + directions[i][0];
+                int nextcol = col + directions[i][1];
+                if(withinBoard(nextrow, nextcol)){
+                    if(board[nextrow][nextcol] == opponent){
+                        while(withinBoard(nextrow, nextcol)){
+                            nextrow = nextrow + directions[i][0];
+                            nextcol = nextcol + directions[i][1];
+                            if(board[nextrow][nextcol] == opponent){
+                                continue;
+                            } else if(board[nextrow][nextcol] == own){
+                                return true;
+                            } else {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    
+    private boolean withinBoard(int row, int col) {
+        if(row >= 0 && col >= 0 && row < board.length && col < board.length){
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean checkMoveString(String move){
+        return Pattern.matches("[a-h]{1}[1-8]{1}", move);
+    }
+
+    public int[] convertStringToCoordinates(String moveStr) {
+        //a=10
+        int[] coordinates = new int[2];
+        coordinates[0] = Character.getNumericValue(moveStr.charAt(1))-1;
+        coordinates[1] = Character.getNumericValue(moveStr.charAt(0))-10;
+        return coordinates;
+    }
+    
+    //for testing
+    public void removeBottomLeftBlack(){
+        this.board[4][3] = 0;
+    }
+    
+}
---
+++ src/test/java/BoardTest.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author riikoro
+ */
+public class BoardTest {
+    
+    private Board board;
+    
+    public BoardTest() {
+    }
+    
+    /*@BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    */
+    @Before
+    public void setUp() {
+        this.board = new Board();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void hello() {}
+    
+    @Test
+    public void initialStateCorrect(){
+        assertEquals(board.getBoard()[2][3], 0);
+        assertEquals(board.getBoard()[3][3], 2);
+        assertEquals(board.getBoard()[4][3], 1);
+    }
+    
+    @Test
+    public void toStringFormatting(){
+        String[] rows = board.toString().split("\n");
+        assertEquals("  a b c d e f g h", rows[0]);
+        assertEquals("4| | | |●|○| | | |", rows[4]);           
+    }
+    
+    @Test
+    public void inputStringChecker(){
+        assert(!board.checkMoveString(":D"));
+        assert(!board.checkMoveString("dd22"));
+        assert(!board.checkMoveString("p9"));
+        assert(board.checkMoveString("h1"));
+    }
+    
+    @Test
+    public void stringToCoordinateConversion(){
+        assertEquals(6, board.convertStringToCoordinates("a7")[0]);
+        assertEquals(2, board.convertStringToCoordinates("c1")[1]);
+    }
+    
+    @Test
+    public void openingMoveOnEdgeWontValidate(){
+        assert(!board.checkMoveValid("h8", 1));
+    }
+    
+    @Test
+    public void noNeighboringOpponentWontValidate(){
+        assert(!board.checkMoveValid("f3", 1));
+    }
+    
+    @Test
+    public void rightOpeningMove(){
+        assert(board.checkMoveValid("d3", 1));
+    }
+    
+    @Test
+    public void noOwnDiscBehindOpponentWontValidate(){
+        board.removeBottomLeftBlack();
+        assert(!board.checkMoveValid("d3", 1));
+    }
+    
+    @Test
+    public void moveAdding(){
+        board.addMove(2, 3, 1);
+        assertEquals(1, board.getBoard()[2][3]);
+        assertEquals(1, board.getBoard()[3][3]);
+    }
+}
+
+
+
