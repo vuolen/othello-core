7118e22880b02d3ceaf5f9e427bdea5f9b8cc1d7
Riikka Korolainen <riikka.korolainen@outlook.com>
2020-09-15 18:18:40 +0300

    win checker tests

--- src/test/java/io/github/vuolen/domain/BoardTest.java
+++ src/test/java/io/github/vuolen/domain/BoardTest.java
@@ -11,54 +11,31 @@ import static org.junit.Assert.*;
  */
 public class BoardTest {
 
-    private Board board;
-
-    public BoardTest() {
-    }
-    
-    /*
-    for debug
-    public String boardToString(int[][] boardToPrint) {
-        String b = "  a b c d e f g h\n";
-        for (int i = 0; i < boardToPrint.length; i++) {
-            b += i + 1;
-            for (int j = 0; j < boardToPrint[0].length; j++) {
-                b += "|";
-                if (boardToPrint[i][j] == 0) {
-                    b += " ";
-                } else if (boardToPrint[i][j] == 1) {
-                    b += "○";
-                } else {
-                    b += "●";
-                }
-            }
-            b += "|\n";
-        }
-        return b;
-    }
-
-    /*@BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-     */
+    private Board openingBoard;
+    private Board endOfGameBoard;
+  
     @Before
     public void setUp() {
-        this.board = new Board();
-    }
-
-    @After
-    public void tearDown() {
+        int[][] boardWithOneMoveLeft = {
+            {2, 2, 2, 2, 1, 2, 1, 1},
+            {2, 2, 1, 2, 1, 2, 2, 2},
+            {2, 1, 1, 1, 2, 2, 2, 2},
+            {2, 1, 1, 1, 1, 1, 2, 2},
+            {2, 2, 2, 1, 1, 1, 2, 2},
+            {1, 1, 1, 1, 1, 1, 1, 2},
+            {2, 2, 2, 2, 1, 1, 1, 2},
+            {1, 1, 1, 1, 2, 2, 2, 0}
+        };
+        
+        this.openingBoard = new Board();
+        this.endOfGameBoard = new Board(boardWithOneMoveLeft);
     }
 
     @Test
     public void initialStateCorrect() {
-        assertEquals(board.getTile(2, 3), EMPTY);
-        assertEquals(board.getTile(3, 3), WHITE);
-        assertEquals(board.getTile(4, 3), BLACK);
+        assertEquals(openingBoard.getTile(2, 3), EMPTY);
+        assertEquals(openingBoard.getTile(3, 3), WHITE);
+        assertEquals(openingBoard.getTile(4, 3), BLACK);
     }
 
     
@@ -75,41 +52,41 @@ public class BoardTest {
 
     @Test
     public void openingMoveOnEdgeWontChangeBoard() {
-        board.addMove(0, 0, WHITE);
-        assertFalse(isBoardChanged(board, new Board()));
+        openingBoard.addMove(0, 0, WHITE);
+        assertFalse(isBoardChanged(openingBoard, new Board()));
     }
     
     @Test
     public void openingMoveOnEdgeReturnsFalse() {
-        assertFalse(board.addMove(0, 0, WHITE));
+        assertFalse(openingBoard.addMove(0, 0, WHITE));
     }
 
     
     @Test
     public void noNeighboringOpponentWontChangeBoard() {
-        board.addMove(2, 5, WHITE);
-        assertFalse(isBoardChanged(board, new Board()));
+        openingBoard.addMove(2, 5, WHITE);
+        assertFalse(isBoardChanged(openingBoard, new Board()));
     }
     
     @Test
     public void noNeighboringOpponentReturnsFalse() {
-        assertFalse(board.addMove(2, 5, WHITE));
+        assertFalse(openingBoard.addMove(2, 5, WHITE));
     }
 
     @Test
     public void rightOpeningMoveReturnsTrue() {
-        assertTrue(board.addMove(3, 2, BLACK));
+        assertTrue(openingBoard.addMove(3, 2, BLACK));
     }
    
     @Test
     public void takenTileMoveReturnsFalse() {
-        board.addMove(3, 2, BLACK);
-        assertFalse(board.addMove(3, 2, BLACK));
+        openingBoard.addMove(3, 2, BLACK);
+        assertFalse(openingBoard.addMove(3, 2, BLACK));
     }
     
     @Test
     public void outOfBoundsMoveReturnsFalse() {
-        assertFalse(board.addMove(-1, -1, BLACK));
+        assertFalse(openingBoard.addMove(-1, -1, BLACK));
     }
     
     @Test
@@ -124,8 +101,8 @@ public class BoardTest {
             {0, 0, 0, 0, 0, 0, 1, 0},
             {0, 0, 0, 0, 0, 0, 0, 0}
         };
-        this.board = new Board(initial);
-        board.addMove(4, 3, BLACK);
+        this.openingBoard = new Board(initial);
+        openingBoard.addMove(4, 3, BLACK);
         
         int[][] expected = {
             {0, 0, 0, 0, 0, 0, 0, 0},
@@ -138,26 +115,27 @@ public class BoardTest {
             {0, 0, 0, 0, 0, 0, 0, 0}
         };
         Board expectedBoard = new Board(expected);
-        assert(board.equals(expectedBoard));
+        assert(openingBoard.equals(expectedBoard));
     }
     
     @Test
     public void simpleMoveIsGameOverDoesNotReturnError() {
         // This produced an ArrayIndexOutOfBounds error
-        board.addMove(3, 2, BLACK);
-        board.addMove(3, 1, WHITE);
-        board.addMove(3, 0, BLACK);
-        assertFalse(board.isGameOver());
+        openingBoard.addMove(3, 2, BLACK);
+        openingBoard.addMove(3, 1, WHITE);
+        openingBoard.addMove(3, 0, BLACK);
+        assertFalse(openingBoard.isGameOver());
     }
     
     @Test
     public void onePossibleMoveLeftGameNotOver() {
-        
+        assert(!endOfGameBoard.isGameOver());
     }
     
     @Test
     public void noPossibleMovesGameOver() {
-        
+        endOfGameBoard.addMove(7, 7, BLACK);
+        assert(endOfGameBoard.isGameOver());
     }
     
     @Test
@@ -180,19 +158,8 @@ public class BoardTest {
     
     @Test
     public void theOneWithMorePiecesAtEndOfGameWins(){
-        
+        endOfGameBoard.addMove(7, 7, BLACK);
+        assertEquals(BLACK, endOfGameBoard.winner());
     }
     
-    /*@Test
-    public void rightOpeningMoveChangesBoard() {
-        board.addMove(3, 2, BLACK);
-        assertEquals(board.getTile(3, 3), BLACK);
-    } */
-
-    /* @Test
-    public void moveAdding(){
-        board.addMove(2, 3, 1);
-        assertEquals(1, board.getBoard()[2][3]);
-        assertEquals(1, board.getBoard()[3][3]);
-    } */
 }
